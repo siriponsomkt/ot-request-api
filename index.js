10b8e6a02129b0674ebe7fecf19ea216418e71e4
B6122683 <64888010+B6122683@users.noreply.github.com>
2022-01-17 21:14:47 +0700

    edit api

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const mysql = require("mysql2");
-const cors = require("cors");
+var express = require("express");
+var mysql = require("mysql2");
+var cors = require("cors");
 
-const bodyParser = require("body-parser");
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
 const cookieParser = require("cookie-parser");
-const session = require("express-session");
+//const session = require("express-session");
 
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const app = express();
+var app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(
@@ -22,17 +24,17 @@ app.use(
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(
-  session({
-    key: "userId",
-    secret: "subscribe",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      expires: 60 * 60 * 24,
-    },
-  })
-);
+// app.use(
+//   session({
+//     key: "userId",
+//     secret: "subscribe",
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       expires: 60 * 60 * 24,
+//     },
+//   })
+// );
 
 const db = mysql.createConnection({
   user: "root",
@@ -41,62 +43,86 @@ const db = mysql.createConnection({
   database: "ot_request",
 });
 
-app.post("/register", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  bcrypt.hash(password, saltRounds, (err, hash) => {
-    if (err) {
-      console.log(err);
-    }
-
-    db.query(
-      "INSERT INTO users (username, password) VALUES (?,?)",
-      [username, hash],
-      (err, result) => {
-        console.log(err);
+//Add Employees (Register)
+app.post("/register", jsonParser, function (req, res) {
+  bcrypt.hash(req.body.emp_password, saltRounds, function (err, hash) {
+    db.execute(
+      "INSERT INTO employees (emp_firstname, emp_surname, emp_address, emp_tel, emp_email, emp_username, emp_password, dep_id, role_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        req.body.emp_firstname,
+        req.body.emp_surname,
+        req.body.emp_address,
+        req.body.emp_tel,
+        req.body.emp_email,
+        req.body.emp_username,
+        hash,
+        req.body.dep_id,
+        req.body.role_id,
+      ],
+      function (err, results, fields) {
+        if (err) {
+          res.json({ status: "error", message: err });
+          return;
+        }
+        res.json({ status: "ok" });
       }
     );
   });
 });
 
-app.get("/login", (req, res) => {
-  if (req.session.user) {
-    res.send({ loggedIn: true, user: req.session.user });
-  } else {
-    res.send({ loggedIn: false });
-  }
-});
-
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
+//LOGIN
+app.post("/login", jsonParser, function (req, res, next) {
   db.execute(
-    "SELECT * FROM users WHERE username = ?;",
-    username,
-    (err, result) => {
+    "SELECT * FROM employees WHERE emp_username= ?",
+    [req.body.emp_username],
+    (err, users, fields) => {
       if (err) {
-        res.send({ err: err });
+        res.json({ status: "error", message: err });
+        return;
       }
-
-      if (result.length > 0) {
-        bcrypt.compare(password, result[0].password, (error, response) => {
-          if (response) {
-            req.session.user = result;
-            console.log(req.session.user);
-            res.send(result);
+      if (users.length == 0) {
+        res.json({ status: "error", message: "no user found" });
+        return;
+      }
+      bcrypt.compare(
+        req.body.emp_password,
+        users[0].emp_password,
+        function (err, result) {
+          if (result) {
+            res.json({ status: "ok", message: "login success" });
+            return;
           } else {
-            res.send({ message: "Wrong username/password combination!" });
+            res.json({ status: "error", message: "login failed" });
           }
-        });
-      } else {
-        res.send({ message: "User doesn't exist" });
-      }
+        }
+      );
     }
   );
 });
 
-app.listen(3883, () => {
-  console.log("running server port 3883");
+// app.post("/login", jsonParser, (req, res) => {
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   db.execute(
+//     "SELECT * FROM users WHERE email = ? && password = ?",
+//     [email, password],
+//     (err, result) => {
+//       if (err) {
+//         res.status(500).send(err);
+//       } else {
+//         if (result.length > 0) {
+//           res.status(200).send({
+//             email: result[0].email,
+//             fname: result[0].fname,
+//           });
+//         } else {
+//           res.status(400).send("user no existe");
+//         }
+//       }
+//     }
+//   );
+// });
+
+app.listen(3333, () => {
+  console.log("running server port 3333");
 });
